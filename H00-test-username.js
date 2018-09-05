3db4489635b345ce237da47bfb1d0b62bae856a6
Aditya Pratap Sngh Rajput <apsrcreatix@gmail.com>
2018-09-05 15:58:35 +0530

    added username file

--- H00-test-username.js
+++ H00-test-username.js
@@ -1,12 +1,23 @@
 // validating username of a person 
 function usernameTest(person){
-    var regex = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
+    // ^ : for first character
+    // [A-za-z0-9] : first should be among these them 
+    // + : matches the preceding item given above one or more time
+    // (?:) : non capturing group
+    // $ : ending
+    var regex = /^[A-Za-z0-9]+(?:[_-][A-Za-z0-9]+)*$/;
     return regex.test(person);
 }
 
-var person = ["apsrcreatix","@ndrodev","$hruti de",".ashish."];
+var person = [
+    "apsrcreatix",
+    "@ndrodev",
+    "$hruti de",
+    ".ashish.",
+    "HEYDUDE",
+    "SINGis%ing"
+];
 
-for(var i in person){
-    console.log(usernameTest(person[i]));
-    console.log(person[i]);
+for(var ith in person){
+    console.log(person[ith] + " : " + usernameTest(person[ith]));
 }
